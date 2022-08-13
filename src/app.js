9034f4556593dc55d60fa7318b370b806212cbb3
OlenaZiri <z.zireael.z@gmail.com>
2022-08-13 23:30:34 +0100

    Display current: temperature, description, humidity, wind speed, city name

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-
+function displayWeather(response) {
+  let cityElement = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#temperature");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#humidity");
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.wind.speed);
+}
+let apiKey = "6ec7cd9ae6e3ca5bb170c503e2a51df0";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayWeather);
