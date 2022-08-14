e94fd22e52709c8e9d850d17cdcf8c60e0c4f667
OlenaZiri <z.zireael.z@gmail.com>
2022-08-14 13:49:58 +0100

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -40,7 +40,16 @@ function displayWeather(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let apiKey = "6ec7cd9ae6e3ca5bb170c503e2a51df0";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayWeather);
+function search(city) {
+  let apiKey = "6ec7cd9ae6e3ca5bb170c503e2a51df0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("New York");
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
